509252a29d1c4e17f11eaa5aaf7ee67176f984d7
oddiseo <ulisescanales08@gmail.com>
2025-09-21 22:25:14 -0600

    Fin primera clase

---
+++ README.md
@@ -0,0 +1,12 @@
+# Flask REST API
+
+API sencilla para gestionar productos. 
+App realizada con tuturiales de FAZT: https://www.youtube.com/watch?v=Esdj9wlBOaI&t=1339s
+
+## Requisitos
+
+- Python 3.11+
+- pip
+- Flask
+- Opcional: virtualenv (`python -m venv`)
+
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify 
+
+app = Flask(__name__) 
+
+from products import products
+
+@app.route('/ping')
+def ping():
+    return jsonify({"message": "pong!"})
+
+@app.route('/products')
+def getProducts():
+    return jsonify({"products": products, "message": "Products list"})
+
+@app.route('/products/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [product for product in products if product["name"] == product_name]
+    if len(productsFound) > 0:
+        return jsonify({"product": productsFound[0]})
+    return jsonify({"message": "Product not found"})
+
+if __name__ == "__main__":
+    app.run(debug=True, port=4000)
+
