83dc8769e715958d2506024a1ec10b86ab4bf550
oddiseo <ulisescanales08@gmail.com>
2025-09-22 11:20:14 -0600

    Fin

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify 
+from flask import Flask, jsonify, request
 
 app = Flask(__name__) 
 
@@ -19,6 +19,40 @@ def getProduct(product_name):
         return jsonify({"product": productsFound[0]})
     return jsonify({"message": "Product not found"})
 
-if __name__ == "__main__":
+@app.route('/products', methods=['POST'])
+def addProduct():
+    new_product = {
+        "name": request.json['name'],
+        "price": request.json['price'],
+        "quantity": request.json['quantity']
+    }
+    products.append(new_product)
+    return jsonify({"message": "Product added successfully", "products": products})
+
+@app.route('/products/<string:product_name>', methods=['PUT'])
+def editProduct(product_name):
+    productsFound = [product for product in products if product["name"] == product_name]
+    if len(productsFound) > 0:
+        productsFound[0]['name'] = request.json['name']
+        productsFound[0]['price'] = request.json['price']
+        productsFound[0]['quantity'] = request.json['quantity']
+        return jsonify({
+            "message": "Product updated",
+            "product": productsFound[0]
+        })
+    return jsonify({"message": "Product not found"})
+
+@app.route('/products/<string:product_name>', methods=['DELETE'])
+def deleteProduct(product_name):
+    productsFound = [product for product in products if product["name"] == product_name]
+    if len(productsFound) > 0:
+        products.remove(productsFound[0])
+        return jsonify({
+            "message": "Product deleted",
+            "products": products
+        })
+    return jsonify({"message": "Product not found"})
+
+if __name__ == "__main__":  
     app.run(debug=True, port=4000)
 
